030da256c8fada95e837b62b06a14417647769f5
daalsoft <daalsoft@gmail.com>
2026-02-15 16:52:56 +0900

    용어 추가2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ function Home() {
     <div className="card">
       <h2>📘 강의 소개</h2>
       <p>
-        Oracle / MySQL / MariaDB / MSSQL / PostgreSQL 성능 비교 및 튜닝 실습
+        Oracle / MySQL 성능 비교 및 튜닝 실습
       </p>
     </div>
   );
